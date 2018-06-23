0eb8250d1806c622d13354f8313d3e371b4d840e
Zach Deibert <zachdeibert@gmail.com>
2018-06-23 11:03:42 -0500

    Added duplicate names log

--- src/main/java/com/github/hanavan99/traincontroller/model/NewObjectConsumer.java
+++ src/main/java/com/github/hanavan99/traincontroller/model/NewObjectConsumer.java
@@ -8,13 +8,19 @@ import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Envelope;
 import com.rabbitmq.client.AMQP.BasicProperties;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class NewObjectConsumer extends TopicConsumer {
+    private static final Logger log = LogManager.getLogger();
     private final CommandableObjectList list;
 
     @Override
     public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body) throws IOException {
         String name = new String(body, "UTF-8");
-        if (!list.exists(name)) {
+        if (list.exists(name)) {
+            log.warn("Object with name '{}' already exists", name);
+        } else {
             list.add(name);
         }
     }
