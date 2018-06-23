a504e3f007d1d3de745ee1be9442f9f7ba3e3d9f
Zach Deibert <zachdeibert@gmail.com>
2018-06-23 03:53:51 -0500

    Switched to underscores

--- src/main/java/com/github/hanavan99/traincontroller/TopicNames.java
+++ src/main/java/com/github/hanavan99/traincontroller/TopicNames.java
@@ -10,10 +10,10 @@ public class TopicNames {
     public static final String ENGINE_COMMAND = "/engine/%s/command/%s";
     public static final String ENGINE_START_COMMAND = "/engine/%s/start/%s";
     public static final String ENGINE_END_COMMAND = "/engine/%s/end/%s";
-    public static final String OBJECT_NEW = "/%s/.new";
+    public static final String OBJECT_NEW = "/%s/_new";
     public static final String OBJECT_DELETE = "/%s/%s/delete";
     public static final String SWITCH_ADD_THROUGH = "/switch/%s/add_route/through";
     public static final String SWITCH_ADD_OUT = "/switch/%s/add_route/out";
     public static final String CLEAR_ROUTE = "/route/%s/clear";
-    public static final String OBJECT_LIST = "/%s/.list";
+    public static final String OBJECT_LIST = "/%s/_list";
 }
