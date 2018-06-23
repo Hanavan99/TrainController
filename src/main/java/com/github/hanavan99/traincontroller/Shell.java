d6a7c0ec789712bf7f1239f9f4ae76bfa0bc3883
Zach Deibert <zachdeibert@gmail.com>
2018-06-23 10:56:27 -0500

    Added command shell

---
+++ src/main/java/com/github/hanavan99/traincontroller/Shell.java
@@ -0,0 +1,43 @@
+package com.github.hanavan99.traincontroller;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.DefaultConsumer;
+import com.rabbitmq.client.Envelope;
+
+public class Shell extends DefaultConsumer {
+    @Override
+    public void handleDelivery(String consumerTag, Envelope envelope, BasicProperties properties, byte[] body) throws IOException {
+        String msg = new String(body, "UTF-8");
+        System.out.println(msg);
+    }
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("192.168.1.247");
+        factory.setVirtualHost("/");
+        factory.setUsername("root");
+        factory.setPassword("root");
+        Connection conn = factory.newConnection();
+        Channel channel = conn.createChannel();
+        String queue = channel.queueDeclare().getQueue();
+        channel.basicConsume(queue, true, new Shell(channel));
+        BasicProperties props = new BasicProperties.Builder().replyTo(queue).build();
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            String[] parts = line.split(" ", 2);
+            byte[] body = parts.length == 2 ? parts[1].getBytes("UTF-8") : new byte[0];
+            channel.basicPublish(parts[0], "", props, body);
+        }
+    }
+
+    Shell(Channel channel) {
+        super(channel);
+    }
+}
